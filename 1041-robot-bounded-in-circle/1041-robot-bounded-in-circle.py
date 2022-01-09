b3401de826d0c7cf8a967658e3f90d4b5589b3f1
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-01-09 22:57:34 +0530

    Time: 43 ms (20.04%), Space: 14.2 MB (49.85%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -2,6 +2,8 @@ class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         x,y = 0,0
         direction = 'n'
+        L = {'n':'w','s':'e','e':'n','w':'s'}
+        R = {'w':'n','e':'s','n':'e','s':'w'}
           
         for _ in range(4):
             for char in instructions:
@@ -15,24 +17,11 @@ class Solution:
                     elif direction == 'w':
                         x -= 1
                 elif char == 'L':
-                    if direction == 'n':
-                        direction = 'w'
-                    elif direction == 's':
-                        direction = 'e'
-                    elif direction == 'e':
-                        direction = 'n'
-                    elif direction == 'w':
-                        direction = 's'
+                    direction = L[direction]
                 elif char == 'R':
-                    if direction == 'w':
-                        direction = 'n'
-                    elif direction == 'e':
-                        direction = 's'
-                    elif direction == 'n':
-                        direction = 'e'
-                    elif direction == 's':
-                        direction = 'w'
-                # print(x,y, direction)
+                    direction = R[direction]
+                    
             if x == 0 and y == 0:
                 return True
+            
         return False
