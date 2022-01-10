d4f2153b4d86cb1e89cc3858c7660a1d4dcced08
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-01-11 00:02:25 +0530

    Time: 32 ms (80.60%), Space: 14.2 MB (80.13%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,12 +1,6 @@
 class Solution:
     def binaryToDecimal(self, num):
-        p = 0
-        ans = 0
-        for i in range(len(num)-1,-1,-1):
-            if num[i] == '1':
-                ans += 2**p
-            p += 1
-        return ans
+        return int(num, 2)
     
     def decimalToBinary(self, num):
         return bin(num).replace("0b", "")
