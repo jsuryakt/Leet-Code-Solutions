e4a27e258779138d60e4951aef35c4fd177ec3e6
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-02-14 23:48:53 +0530

    Time: 1510 ms (47.46%), Space: 14 MB (89.53%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,44 +1,32 @@
-# class Solution:
-#     allPerms = []
-#     def getAllPerms(self, string):
-        
-#     def longestPalindrome(self, s: str) -> str:
-#         def checkPalindrome(self, string):
-#             start = 0
-#             end = len(string) - 1
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            currChar = s[i]
             
-#             while(start<end):
-#                 if string[start] != string[end]:
-#                     return False
-#                 start += 1
-#                 end -= 1
-#             return True
-        
-#         maxLen = -1
-#         allPerms = self.getAllPerms(s)
-#         for word in allPerms:
-#             if checkPalindrome(word):
-#                 lengthOfPldrm = len(word)
-#                 maxLen = max(maxLen, lengthOfPldrm)
-
-class Solution :
-    lo = 0
-    maxLen = 0
-    def  longestPalindrome(self, s) :
-        length = len(s)
-        if (length < 2) :
-            return s
-        i = 0
-        while (i < length - 1) :
-            self.extendPalindrome(s, i, i)
-            # assume odd length, try to extend Palindrome as possible
-            self.extendPalindrome(s, i, i + 1)
-            i += 1
-        return s[self.lo:self.lo + self.maxLen]
-    def extendPalindrome(self, s,  j,  k) :
-        while (j >= 0 and k < len(s) and s[j] == s[k]) :
-            j -= 1
-            k += 1
-        if (self.maxLen < k - j - 1) :
-            self.lo = j + 1
-            self.maxLen = k - j - 1
+            left = i
+            right = i
+            
+            while(left >= 0 and right<len(s) and s[left] == s[right]):
+                left -= 1
+                right += 1
+            
+            oddLen = right - left - 1
+            
+            left = i
+            right = i+1
+            
+            while(left >= 0 and right<len(s) and s[left] == s[right]):
+                left -= 1
+                right += 1
+                
+            evenLen = right - left - 1
+            
+            maxLen = max(evenLen, oddLen)
+            
+            if maxLen > end-start:
+                start = i - (maxLen-1)//2
+                end = i + maxLen//2
+        return s[start:end+1]
+            
+                
