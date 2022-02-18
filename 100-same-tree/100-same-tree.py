ab1cb23e56da5da40f1f649d810d8bf535685a9f
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-02-19 01:20:17 +0530

    Time: 46 ms (40.18%), Space: 13.9 MB (75.42%) - LeetHub

---
+++ 100-same-tree/100-same-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorder(self, root1, root2):
+        
+        if root1 == None and root2 == None:
+            return True
+        
+        if root1 == None or root2 == None:
+            return False
+        
+        left = self.inorder(root1.left, root2.left)
+        
+        if root1.val != root2.val:
+            return False
+        
+        right = self.inorder(root1.right, root2.right)
+        
+        return left and right
+        
+    
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.inorder(p,q)
