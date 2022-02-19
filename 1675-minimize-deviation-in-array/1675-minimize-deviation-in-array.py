aa8c9420a1874dba10d424548a1200516a2786bb
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-02-20 01:58:57 +0530

    Time: 1512 ms (47.57%), Space: 26.3 MB (82.52%) - LeetHub

---
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -0,0 +1,50 @@
+import heapq
+class Solution:
+    def findMinMaxIdx(self, nums):
+        minnIdx = 0
+        maxxIdx = 0
+        for i in range(len(nums)):
+            ele = nums[i]
+            if ele > nums[maxxIdx]:
+                maxxIdx = i
+            if ele < nums[minnIdx]:
+                minnIdx = i
+        return [minnIdx, maxxIdx]
+    
+    def isEven(self, num):
+        return num%2 == 0
+    
+    def isOdd(self, num):
+        return num%2 != 0
+    
+    def minimumDeviation(self, nums: List[int]) -> int:
+        minEle, maxEle = 9999999999, -999999999
+        
+        for i in range(len(nums)):
+            if self.isOdd(nums[i]):
+                nums[i] = nums[i] * 2
+            if nums[i] < minEle:
+                minEle = nums[i]
+            if nums[i] > maxEle:
+                maxEle = nums[i]
+            nums[i] = nums[i] * -1
+        
+        ans = maxEle - minEle
+        
+        heapq.heapify(nums)
+    
+        while(self.isEven(nums[0])):
+            heapMax = heapq.heappop(nums) * -1
+            
+            ans = min(ans, heapMax - minEle)
+                
+            heapMax = heapMax//2
+            
+            if heapMax < minEle:
+                minEle = heapMax
+
+            heapq.heappush(nums, -1*heapMax)
+                
+        heapMax = heapq.heappop(nums) * -1
+        ans = min(ans, heapMax - minEle)
+        return ans
