645f42e395a7ac6f98dc67bf520335760eab494d
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-02-28 22:35:30 +0530

    Time: 58 ms (13.09%), Space: 13.9 MB (61.40%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.py
@@ -0,0 +1,27 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if len(nums) == 0:
+            return []
+        i = 0
+        chainStart = nums[0]
+        chainEnd = nums[0]
+        ans = []
+        
+        while(i<len(nums)-1):
+            # while(nums[i] == nums[i+1]):
+            #     i += 1
+            if nums[i]+1 == nums[i+1]:
+                chainEnd = nums[i+1]
+            else:
+                if chainStart != chainEnd:
+                    ans.append(str(chainStart)+"->"+str(chainEnd))
+                else:
+                    ans.append(str(chainStart))
+                chainStart = nums[i+1]
+                chainEnd = nums[i+1]
+            i += 1
+        if chainStart != chainEnd:
+            ans.append(str(chainStart)+"->"+str(chainEnd))
+        else:
+            ans.append(str(chainStart))
+        return ans
