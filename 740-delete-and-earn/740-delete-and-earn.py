57d105388637a16df9a08c5296967d8cca63a445
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-03-06 02:23:43 +0530

    Time: 65 ms (75.85%), Space: 14.2 MB (65.62%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,21 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        lengthOfFreq = max(nums)+1
+        freq = [0]*lengthOfFreq
+        
+        for ele in nums:
+            freq[ele] += ele
+            
+        take = 0
+        skip = 0
+        
+        for i in range(lengthOfFreq):
+            takeIt = freq[i] + skip
+            skipIt = max(take,skip)
+            
+            take = takeIt
+            skip = skipIt
+        return max(take, skip)
+            
+            
+            
