3208cedcb6ed2dc13310b60d0635caef5d113c27
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-03-07 01:08:21 +0530

    Time: 41 ms (72.54%), Space: 13.8 MB (87.46%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,16 +1,10 @@
-class Solution:
-    def factorial(self,n):
-        ans = 1
-        for i in range(1,n+1):
-            ans = ans*i
-        return ans
-    
+class Solution: 
     def countOrders(self, n: int) -> int:
         order = n
         totalSpaces = order*2
         
-        finalAns = self.factorial(order)%(10**9+7)
+        finalAns = 1
         for i in range(1, order+1):
-            finalAns = (finalAns * (2*i - 1))%(10**9+7)
+            finalAns = (finalAns * (2*i - 1)*i)%(10**9+7)
         return finalAns%(10**9+7)
             
