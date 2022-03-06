c2e2f3bb610140b2a782369dbbd3b97ce789e91f
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-03-07 01:09:03 +0530

    Time: 52 ms (47.46%), Space: 13.9 MB (69.25%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,10 +1,9 @@
 class Solution: 
     def countOrders(self, n: int) -> int:
         order = n
-        totalSpaces = order*2
         
         finalAns = 1
         for i in range(1, order+1):
-            finalAns = (finalAns * (2*i - 1)*i)%(10**9+7)
+            finalAns = (finalAns * (2*i - 1)*i)
         return finalAns%(10**9+7)
             
