65aab770c963421c04fd2a81af106c6e960f7b2e
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-03-17 00:57:11 +0530

    Time: 62 ms (12.25%), Space: 14.1 MB (34.75%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateK(self, head, k):
+        tail = head
+        prev = None
+        while(head != None and k>0):
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+            k -= 1
+        newHead = prev
+        return [newHead, tail, head]
+    
+    def lengthOfList(self, head):
+        length = 0
+        
+        while(head != None):
+            head = head.next
+            length += 1
+        return length
+    
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right:
+            return head
+        oldHead = head
+        k = right-left+1
+        tempLeft = left
+        length = self.lengthOfList(head)
+        
+        i = 1
+        while(head != None and i<left):
+            tempHead = head
+            head = head.next
+            i += 1
+        newHead, tail, oldTail = self.rotateK(head, k)
+        # print(tempHead.val)
+        # print(newHead.val)
+        # print(tail.val)
+        # print(oldTail.val)
+        
+        tail.next = oldTail
+        if k == length or tempLeft == 1:
+            return newHead
+        else:
+            tempHead.next = newHead
+        return oldHead
+        
