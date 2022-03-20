dc45c31861541452db32b950f4b5f4cb62824dc1
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-03-20 23:46:32 +0530

    Time: 1208 ms (82.04%), Space: 15 MB (77.02%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,48 +1,64 @@
 class Solution:
     # O(n)
-    def findSwaps(self, arr1, arr2, freq, ele):
-        swaps = 0
-        count = 0
-        for i in range(len(arr1)):
-            curr = arr1[i]
-            if curr != ele:
-                if ele == arr2[i]:
-                    swaps += 1
-            else:
-                count += 1
-        if swaps == 0 and count != len(arr1) or swaps+freq != len(arr1):
-            return -1
-        return swaps
+    # def findSwaps(self, arr1, arr2, freq, ele):
+    #     swaps = 0
+    #     count = 0
+    #     for i in range(len(arr1)):
+    #         curr = arr1[i]
+    #         if curr != ele:
+    #             if ele == arr2[i]:
+    #                 swaps += 1
+    #         else:
+    #             count += 1
+    #     if swaps == 0 and count != len(arr1) or swaps+freq != len(arr1):
+    #         return -1
+    #     return swaps
                 
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         length = len(tops)
         map1 = [0]*7
         map2 = [0]*7
+        mapSame = [0]*7
+        
+        # 4
+        # 2
+        # min(2,4) 2+4 == length
         
         # O(n)
         for i in range(length):
             map1[tops[i]] = map1[tops[i]] + 1
             map2[bottoms[i]] = map2[bottoms[i]] + 1
+            if tops[i] == bottoms[i]:
+                mapSame[tops[i]] = mapSame[tops[i]] + 1
         
-        maxFreq1 = 0
-        maxEle1 = 0
-        maxFreq2 = 0
-        maxEle2 = 0
+        ans = 0
+        for i in range(7):
+            if map1[i] == length or map2[i] ==length:
+                return 0
+            if map1[i]+map2[i]-mapSame[i] == length:
+                ans = min(map1[i], map2[i])-mapSame[i]
+                return ans
+        return -1
         
-        # O(7*1) - O(1)
-        for i in range(1,7):
-            if map1[i] > maxFreq1:
-                maxFreq1 = map1[i]
-                maxEle1 = i
-            if map2[i] > maxFreq2:
-                maxFreq2 = map2[i]
-                maxEle2 = i
+#         maxFreq1 = 0
+#         maxEle1 = 0
+#         maxFreq2 = 0
+#         maxEle2 = 0
+        
+#         # O(7*1) - O(1)
+#         for i in range(1,7):
+#             if map1[i] > maxFreq1:
+#                 maxFreq1 = map1[i]
+#                 maxEle1 = i
+#             if map2[i] > maxFreq2:
+#                 maxFreq2 = map2[i]
+#                 maxEle2 = i
 
-        if maxFreq1 >= maxFreq2:
-            ans = self.findSwaps(tops, bottoms, maxFreq1, maxEle1)
-        else:
-            ans = self.findSwaps(bottoms, tops, maxFreq2, maxEle2)
-        return ans
+#         if maxFreq1 >= maxFreq2:
+#             ans = self.findSwaps(tops, bottoms, maxFreq1, maxEle1)
+#         else:
+#             ans = self.findSwaps(bottoms, tops, maxFreq2, maxEle2)
+#         return ans
         
         # TC - O(5n) - O(n)
         # SC - O(2n) - O(n)
