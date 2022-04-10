ee3410acd74fa5e7b97744fa34482cdd95c40289
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-04-10 23:32:48 +0530

    Time: 44 ms (84.23%), Space: 14.1 MB (34.49%) - LeetHub

--- 682-baseball-game/682-baseball-game.py
+++ 682-baseball-game/682-baseball-game.py
@@ -1,25 +1,19 @@
 class Solution:
     def calPoints(self, ops: List[str]) -> int:
         stack = []
-        top = -1
         
         for char in ops:
             if char == '+':
                 firstTop = stack[-1]
                 secondTop = stack[-2]
                 stack.append(firstTop+secondTop)
-                top -= 1
             elif char == 'C':
                 stack.pop()
-                top -= 1
             elif char == 'D':
                 newTop = stack[-1]*2
                 stack.append(newTop)
-                top += 1
             else:
                 stack.append(int(char))
-                top += 1
-            # print(stack)
         ans = 0
         for ele in stack:
             ans += ele
