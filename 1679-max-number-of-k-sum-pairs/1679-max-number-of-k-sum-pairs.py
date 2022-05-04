017b8961b055fdca5dc5d6f311c06015312bea86
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-05-04 23:36:44 +0530

    Time: 1253 ms (6.48%), Space: 27.1 MB (33.48%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -3,10 +3,9 @@ class Solution:
         dic = {}
         count = 0
         for ele in nums:
-            # print(dic)
             if k-ele in dic and dic[k-ele] != 0:
                 dic[k-ele] -= 1
                 count += 1
-                dic[ele] = dic.get(ele, 0) - 1 
-            dic[ele] = dic.get(ele, 0) + 1
+            else:
+                dic[ele] = dic.get(ele, 0) + 1
         return count
