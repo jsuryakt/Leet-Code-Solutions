3cdad634b35ad59b22f111d7c8bdeb266dda535e
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-07-28 10:44:21 -0700

    Time: 3 ms (95.30%), Space: 42.8 MB (86.26%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] charArr = new int[26];
-        String smallest = t;
-        String other = s;
-        if(s.length()<t.length()) {
-            smallest = s;
-            other = t;
+        for(int i=0; i<s.length(); i++) {
+            charArr[s.charAt(i)-'a']++;
         }
-        for(int i=0; i<smallest.length(); i++) {
-            charArr[smallest.charAt(i)-'a']++;
-        }
-        for(int i=0; i<other.length(); i++) {
-            charArr[other.charAt(i)-'a']--;
+        for(int i=0; i<t.length(); i++) {
+            charArr[t.charAt(i)-'a']--;
         }
         for(int ele:charArr) {
             if(ele != 0) {
