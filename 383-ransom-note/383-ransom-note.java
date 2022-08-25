313926bfb161ea7585e4740acd687e77e7c12d6c
Jayasurya <63441093+jsuryakt@users.noreply.github.com>
2022-08-25 22:47:05 +0530

    Time: 28 ms (33.67%), Space: 51.6 MB (37.52%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> charMap = new HashMap<>();
+        HashMap<Integer, Integer> charMap = new HashMap<>();
         for(int idx=0; idx<magazine.length(); idx++) {
-            char currChar = magazine.charAt(idx);
+            int currChar = getASCII(magazine.charAt(idx));
             charMap.put(currChar, charMap.getOrDefault(currChar, 0)+1);
         }
         
         for(int idx=0; idx<ransomNote.length();idx++) {
-            char currChar = ransomNote.charAt(idx);
+            int currChar = getASCII(ransomNote.charAt(idx));
             int charFreq = charMap.getOrDefault(currChar, 0);
             if(charFreq == 0) {
                 return false;
@@ -15,5 +15,9 @@ class Solution {
             charMap.put(currChar, --charFreq);
         }
         return true;
+    }
+    
+    public int getASCII(char ch) {
+        return ch-97;
     }
 }
